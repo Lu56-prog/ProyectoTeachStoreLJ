7d22e51d3568d7a3ffd984ac39107e20ee98ae33
Lu56-prog <lucerocardona167@gmail.com>
2025-10-17 13:21:09 -0500

    Corregir X2

--- src/main/java/Clases/OrdenCompra.java
+++ src/main/java/Clases/OrdenCompra.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
 import java.awt.List;
 
-/**
- *
- * @author 90W
- */
+//Falta heredar de Venta y coregir listas
 public class OrdenCompra {
     private List<String> listaProductosFisicos;
     private List<String> listaProductosDigitales;
